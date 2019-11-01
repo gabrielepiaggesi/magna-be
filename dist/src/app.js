6153b663cf470d3bdb864df0679f954d1b1950ea
gabrielepiaggesi <gabriele.piaggesi@togetherprice.com>
2019-11-01 15:03:38 +0100

    dynamic port

--- dist/src/app.js
+++ dist/src/app.js
@@ -11,16 +11,13 @@ const api_1 = __importDefault(require("./integration/api"));
 const index_1 = require("./integration/middleware/index");
 // initialize
 const app = express_1.default();
-const port = 3000;
+const port = process.env.PORT || 3000;
 connection_1.connect();
 index_1.initMiddlewares(app);
 app.use(body_parser_1.default.json());
 app.use(api_1.default);
 // start server
-app.listen(port, (err) => {
-    if (err) {
-        return console.error(err);
-    }
+app.listen(port, () => {
     return console.log(`server listen on ${port}`);
 });
 //# sourceMappingURL=app.js.map
