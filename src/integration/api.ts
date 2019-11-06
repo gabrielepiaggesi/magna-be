50b13d0359208f71576199ab4cfe929baf4a9b2c
gabrielepiaggesi <gabriele.piaggesi@togetherprice.com>
2019-11-06 22:21:38 +0100

    get creator info

--- src/integration/api.ts
+++ src/integration/api.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./AuthApi";
+import creatorRoutes from "./CreatorApi";
 import stockRoutes from "./StockApi";
 import userRoutes from "./UserApi";
 const routes = express.Router();
@@ -7,5 +8,6 @@ const routes = express.Router();
 routes.use("/auth", authRoutes);
 routes.use("/users", userRoutes);
 routes.use("/stocks", stockRoutes);
+routes.use("/creators", creatorRoutes);
 
 export default routes;
