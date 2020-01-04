f307af0e351f4f4983beaa8777e5fd7b3e6e75fb
gabrielepiaggesi <gabriele.piaggesi@togetherprice.com>
2020-01-04 12:49:16 +0100

    add stories routes and feed endpoint

--- src/integration/StoryApi.ts
+++ src/integration/StoryApi.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import { auth } from "./middleware/index";
 import { StoryService } from "../services/StoryService";
-const stockRoutes = express.Router();
+const storyRoutes = express.Router();
 
 // services
 const storyService = new StoryService();
 
 // routes
-stockRoutes.post("/create", auth.isUser, async (req, res) => await storyService.createStory(res, req.body));
-stockRoutes.post("/edit", auth.isUser, async (req, res) => await storyService.editStory(res, req.body));
-stockRoutes.delete("/:id", auth.isUser, async (req, res) => await storyService.deleteStory(res, parseInt(req.params.id, 10)));
-stockRoutes.get("/:id", auth.isUser, async (req, res) => await storyService.getStory(res, parseInt(req.params.id, 10)));
+storyRoutes.post("/create", auth.isUser, async (req, res) => await storyService.createStory(res, req.body));
+storyRoutes.post("/edit", auth.isUser, async (req, res) => await storyService.editStory(res, req.body));
+storyRoutes.delete("/:id", auth.isUser, async (req, res) => await storyService.deleteStory(res, parseInt(req.params.id, 10)));
+storyRoutes.get("/:id", auth.isUser, async (req, res) => await storyService.getStory(res, parseInt(req.params.id, 10)));
+storyRoutes.get("/feed/:id", auth.isUser, async (req, res) => await storyService.getStory(res, parseInt(req.params.id, 10)));
 
-export default stockRoutes;
+export default storyRoutes;
--- src/integration/api.ts
+++ src/integration/api.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import authRoutes from "./AuthApi";
-import creatorRoutes from "./CreatorApi";
-import stockRoutes from "./StockApi";
 import userRoutes from "./UserApi";
+import storyRoutes from "./StoryApi";
 const routes = express.Router();
 
 routes.use("/auth", authRoutes);
 routes.use("/users", userRoutes);
-routes.use("/stocks", stockRoutes);
-routes.use("/creators", creatorRoutes);
+routes.use("/stories", storyRoutes);
 
 export default routes;
