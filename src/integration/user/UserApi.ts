ea3d79b76a6cb209f8952cab77fc84a4394b9e68
gabrielepiaggesi <gabriele.piaggesi@togetherprice.com>
2020-05-18 22:03:12 +0200

    test saveandlock.com

--- src/integration/user/UserApi.ts
+++ src/integration/user/UserApi.ts
@@ -11,6 +11,6 @@ userRoutes.get("/me", auth.isUser, async (req, res) => await userService.getLogg
 userRoutes.get("/:id", auth.isUser, async (req, res) => await userService.getUser(res, parseInt(req.params.id, 10)));
 userRoutes.post("/create", auth.isUser, async (req, res) => await userService.createUser(res, req.body));
 
-userRoutes.post("/save/email", async (req, res) => await userService.saveEmail(res, req.body));
+userRoutes.post("/save", async (req, res) => await userService.saveEmail(res, req.body));
 
 export default userRoutes;
