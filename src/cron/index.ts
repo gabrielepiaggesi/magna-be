89aa4102b7b8e1a5a50e636cbbf8756035771b2c
Gabriele Piaggesi <piaggesigabriele@gmail.com>
2020-10-21 18:11:58 +0200

    financial

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -7,9 +7,9 @@ const LOG = new Logger("CRON");
 
 export const initJobs = (app) => {
     var retentionEmailJob = 
-    new CronJob('0 10 18 * * *', async function() {
+    new CronJob('0 30 18 * * *', async function() {
         LOG.info('retentionEmailJob started');
-        // await EmailSender.sendRetentionEmail();
+        await EmailSender.sendRetentionEmail();
         LOG.info('retentionEmailJob completed');
     }, null, true, 'Europe/Rome');
     retentionEmailJob.start();
