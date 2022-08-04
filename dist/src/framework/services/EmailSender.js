b53af0563ce1fb2351573f0c8a6f88bd89c63dd1
Gabriele Piaggesi <piaggesigabriele@gmail.com>
2022-08-04 15:38:04 +0200

    fix

--- dist/src/framework/services/EmailSender.js
+++ dist/src/framework/services/EmailSender.js
@@ -84,10 +84,26 @@ class EmailSender {
                 LOG.error(error);
                 LOG.error(response);
             }
-            LOG.debug(options.body);
+            LOG.debug(options);
             LOG.debug(body);
         });
     }
 }
 exports.EmailSender = EmailSender;
+// var SibApiV3Sdk = require('sib-api-v3-sdk');
+// SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = 'YOUR_API_KEY';
+// new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail(
+//   {
+//     'subject':'Hello from the Node SDK!',
+//     'sender' : {'email':'api@sendinblue.com', 'name':'Sendinblue'},
+//     'replyTo' : {'email':'api@sendinblue.com', 'name':'Sendinblue'},
+//     'to' : [{'name': 'John Doe', 'email':'example@example.com'}],
+//     'htmlContent' : '<html><body><h1>This is a transactional email {{params.bodyMessage}}</h1></body></html>',
+//     'params' : {'bodyMessage':'Made just for you!'}
+//   }
+// ).then(function(data) {
+//   console.log(data);
+// }, function(error) {
+//   console.error(error);
+// });
 //# sourceMappingURL=EmailSender.js.map
