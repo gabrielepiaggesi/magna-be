10c7d60bf1e132342d467102f80f644c3b183103
Gabriele Piaggesi <piaggesigabriele@gmail.com>
2022-08-04 15:59:56 +0200

    rm git

--- env/dev/apikeys.ts
+++
@@ -1,3 +0,0 @@
-export const apikeys = {
-    SENDINBLUE_API_KEY: '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'
-};
--- env/dev/jwt.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConfig = {
-    secretOrKey: "IndroUser122828?",
-};
