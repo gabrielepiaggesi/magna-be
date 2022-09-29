b048f2a7a237efc7d1a526de2aa08705b422f671
Gabriele Piaggesi <piaggesigabriele@gmail.com>
2022-09-29 18:24:42 +0200

    obsolete_version on db

--- src/mgn-entity/model/AppVersion.ts
+++ src/mgn-entity/model/AppVersion.ts
@@ -2,6 +2,7 @@ import { Table } from "../../mgn-framework/models/Table";
 
 export class AppVersion extends Table {
     public version_number: string;
+    public obsolete_version: string;
     public check_version: number;
 
     // tslint:disable-next-line:no-empty
